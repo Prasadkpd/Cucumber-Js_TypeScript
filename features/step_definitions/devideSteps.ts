225e6fb09fd06fb3fe90a2bc3d82efbfd9f02ad3
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-11-10 10:11:24 +0530

    changes

--- features/step_definitions/devideSteps.ts
+++ features/step_definitions/devideSteps.ts
@@ -1,21 +1,23 @@
 import { When, Then, Given } from "@cucumber/cucumber";
 import {expect} from "chai";
+import { Calculator } from "../../src/calculator";
 
-Given("a calculator", function () {
-    this.calculator = {
-        divide(n1: number, n2: number): number {
-            return n1/n2;
-        }
-    }
+interface World {
+    calculator: Calculator;
+    actual: number;
+}
+
+Given("a calculator", function (this: World) {
+    this.calculator = new Calculator();
 });
 
 When("I want to divide (int) by (int)", function(n1:number, n2: number) {
    this.actual = this.calculator.divide(n1, n2); 
 });
 
-// Then("the result is (int)", function(expected: number) {
-//     expect(this.actual).to.be.equal(expected);
-// });
+Then("the result is (int)", function(expected: number) {
+    expect(this.actual).to.be.equal(expected);
+});
 
 Then('the result is {int}', (int: number) => {
     Then('the result is {float}', (float: number) => {
---
+++ src/calculator.ts
@@ -0,0 +1,9 @@
+export class Calculator {
+    divide(n1: number, n2: number): number {
+        return n1 / n2;
+    }
+
+    add(n1: number, n2: number): number {
+        return n1 + n2;
+    }
+}
